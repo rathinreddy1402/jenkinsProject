74f309ea9b5e65c0581fdd845bd02bf7c21a2f4b
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-01-29 12:34:33 +0530

    3rd commit

--- jenkinsExe/src/test/java/demo/SampleTest.java
+++ jenkinsExe/src/test/java/demo/SampleTest.java
@@ -4,7 +4,8 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("welcome");
+		System.out.println("bye");
 	}
 
 }
